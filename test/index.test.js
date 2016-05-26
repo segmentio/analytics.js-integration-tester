7340234666f33e30f0b27116f934b491f3c915d1
Nathan Houle <nathan+github@nathanhoule.com>
2016-05-25 18:13:20 -0700

    Modernize test harness; remove Duo support, add Browserify support (#34)

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,12 @@
+'use strict';
 
-var createIntegration = require('analytics.js-integration');
-var Analytics = require('analytics.js-core').constructor;
+var Analytics = require('@segment/analytics.js-core').constructor;
 var assert = require('assert');
-var facade = require('facade');
-var tester = require('../');
+var createIntegration = require('@segment/analytics.js-integration');
+var tester = require('../lib');
 
-describe('integration-tester', function(){
-  var noop = function(){};
+describe('integration-tester', function() {
+  var noop = function() {};
   var analytics;
   var integration;
   var Integration = createIntegration('Name')
@@ -16,14 +16,14 @@ describe('integration-tester', function(){
     .mapping('map')
     .readyOnLoad();
 
-  beforeEach(function(){
+  beforeEach(function() {
     analytics = new Analytics;
     integration = new Integration;
     analytics.use(tester);
     analytics.add(integration);
   });
 
-  it('should compare two integrations', function(){
+  it('should compare two integrations', function() {
     analytics.compare(Integration, createIntegration('Name')
       .global('global')
       .option('option', 'value')
@@ -32,23 +32,23 @@ describe('integration-tester', function(){
       .readyOnLoad());
   });
 
-  describe('#spy', function(){
-    it('should spy on a method of a host object', function(){
+  describe('#spy', function() {
+    it('should spy on a method of a host object', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       assert.notEqual(obj.method, noop);
     });
   });
 
-  describe('#stub', function(){
-    it('should stub a method of a host object', function(){
+  describe('#stub', function() {
+    it('should stub a method of a host object', function() {
       var obj = { method: noop };
       analytics.stub(obj, 'method');
       assert.notEqual(obj.method, noop);
       assert.equal(typeof obj.method, 'function');
     });
 
-    it('should restore a stubbed method', function(){
+    it('should restore a stubbed method', function() {
       var obj = { method: noop };
       analytics.stub(obj, 'method');
       analytics.restore(obj, 'method');
@@ -56,38 +56,38 @@ describe('integration-tester', function(){
     });
   });
 
-  describe('#called', function(){
-    it('should throw if the spy does not exist', function(){
-      assert.throws(function(){
+  describe('#called', function() {
+    it('should throw if the spy does not exist', function() {
+      assert['throws'](function() {
         analytics.called(noop);
       });
     });
 
-    it('should throw if the spy was not called', function(){
+    it('should throw if the spy was not called', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.called(obj.method);
       });
     });
 
-    it('should not throw if the spy was called', function(){
+    it('should not throw if the spy was called', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method();
       analytics.called(obj.method);
     });
 
-    it('should throw if the spy was not called with the right arguments', function(){
+    it('should throw if the spy was not called with the right arguments', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method('a');
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.called(obj.method, 'b');
       });
     });
 
-    it('should not throw if the spy was called with the right arguments', function(){
+    it('should not throw if the spy was called with the right arguments', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method('a');
@@ -95,38 +95,38 @@ describe('integration-tester', function(){
     });
   });
 
-  describe('#didNotCall', function(){
-    it('should throw if the spy does not exist', function(){
-      assert.throws(function(){
+  describe('#didNotCall', function() {
+    it('should throw if the spy does not exist', function() {
+      assert['throws'](function() {
         analytics.didNotCall(noop);
       });
     });
 
-    it('should throw if the spy was called', function(){
+    it('should throw if the spy was called', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method();
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.didNotCall(obj.method);
       });
     });
 
-    it('should not throw is the spy was not called', function(){
+    it('should not throw is the spy was not called', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       analytics.didNotCall(obj.method);
     });
 
-    it('should throw if the spy was called with the right arguments', function(){
+    it('should throw if the spy was called with the right arguments', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method('a');
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.didNotCall(obj.method, 'a');
       });
     });
 
-    it('should not throw if the spy was called with the wrong arguments', function(){
+    it('should not throw if the spy was called with the wrong arguments', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method('a');
@@ -134,47 +134,47 @@ describe('integration-tester', function(){
     });
   });
 
-  describe('#returned', function(){
-    it('should throw if the spy does not exist', function(){
-      assert.throws(function(){
+  describe('#returned', function() {
+    it('should throw if the spy does not exist', function() {
+      assert['throws'](function() {
         analytics.returned(noop);
       });
     });
 
-    it('should throw if the spy did not return the value', function(){
+    it('should throw if the spy did not return the value', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method();
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.returned(obj.method, 'a');
       });
     });
 
-    it('should not throw if the spy returned the value', function(){
-      var obj = { method: function(){ return 1; }};
+    it('should not throw if the spy returned the value', function() {
+      var obj = { method: function() { return 1; } };
       analytics.spy(obj, 'method');
       obj.method();
       analytics.returned(obj.method, 1);
     });
   });
 
-  describe('#didNotReturn', function(){
-    it('should throw if the spy does not exist', function(){
-      assert.throws(function(){
+  describe('#didNotReturn', function() {
+    it('should throw if the spy does not exist', function() {
+      assert['throws'](function() {
         analytics.didNotReturn(noop);
       });
     });
 
-    it('should throw if the spy returned the value', function(){
-      var obj = { method: function(){ return 1; }};
+    it('should throw if the spy returned the value', function() {
+      var obj = { method: function() { return 1; } };
       analytics.spy(obj, 'method');
       obj.method();
-      assert.throws(function(){
+      assert['throws'](function() {
         analytics.didNotReturn(obj.method, 1);
       });
     });
 
-    it('should not throw if the spy did not return the value', function(){
+    it('should not throw if the spy did not return the value', function() {
       var obj = { method: noop };
       analytics.spy(obj, 'method');
       obj.method();
@@ -182,80 +182,80 @@ describe('integration-tester', function(){
     });
   });
 
-  describe('#assert', function(){
-    it('should throw with a falsey value', function(){
-      assert.throws(function(){
+  describe('#assert', function() {
+    it('should throw with a falsey value', function() {
+      assert['throws'](function() {
         analytics.assert(false);
       });
     });
 
-    it('should not throw with a truthy value', function(){
+    it('should not throw with a truthy value', function() {
       analytics.assert(true);
     });
   });
 
-  describe('#equal', function(){
-    it('should throw if the values are not equal', function(){
-      assert.throws(function(){
+  describe('#equal', function() {
+    it('should throw if the values are not equal', function() {
+      assert['throws'](function() {
         analytics.equal('a', 'b');
       });
     });
 
-    it('should not throw if the values are equal', function(){
+    it('should not throw if the values are equal', function() {
       analytics.equal('a', 'a');
     });
   });
 
-  describe('#deepEqual', function(){
-    it('should throw if the values are not deep equal', function(){
-      assert.throws(function(){
+  describe('#deepEqual', function() {
+    it('should throw if the values are not deep equal', function() {
+      assert['throws'](function() {
         analytics.deepEqual({}, { baz: true });
       });
     });
 
-    it('should not throw if the values are deep equal', function(){
+    it('should not throw if the values are deep equal', function() {
       analytics.deepEqual({}, {});
     });
   });
 
-  describe('#loaded', function(){
-    beforeEach(function(){
-      Integration.tag('example-img', '<img src="http://example.com/{{name}}.png">')
+  describe('#loaded', function() {
+    beforeEach(function() {
+      Integration.tag('example-img', '<img src="http://example.com/{{name}}.png">');
       Integration.tag('example-script', '<script src="http://ajax.googleapis.com/ajax/libs/jquery/{{version}}/jquery.min.js"></script>');
-      analytics = new Analytics;
-      integration = new Integration;
+      analytics = new Analytics();
+      integration = new Integration();
       analytics.use(tester);
       analytics.add(integration);
       analytics.spy(integration, 'load');
     });
 
-    describe('img tag', function(){
-      it('should throw if it does not find an img tag', function(){
-        assert.throws(function(){
+    describe('img tag', function() {
+      it('should throw if it does not find an img tag', function() {
+        assert['throws'](function() {
           analytics.loaded('<img src="http://example.com/example.png"/>');
         });
       });
- 
-      it('should not throw if it does find an Image', function(){
+
+      it('should not throw if it does find an Image', function() {
         integration.load('example-img', { name: 'example' });
         analytics.loaded('<img src="http://example.com/example.png"/>');
       });
     });
- 
-    describe('script tag', function(){
-      it('should throw if it does not find a script tag', function(){
-        assert.throws(function(){
+
+    describe('script tag', function() {
+      it('should throw if it does not find a script tag', function() {
+        assert['throws'](function() {
           analytics.loaded('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>');
         });
       });
- 
-      it('should not throw if it does find a script tag', function(){
+
+      it('should not throw if it does find a script tag', function() {
         integration.load('example-script', { version: '1.11.1' });
         analytics.loaded('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>');
       });
     });
 
-    it('should accept integration argument', function(){
+    it('should accept integration argument', function() {
       integration.load('example-img', { name: 'example' });
       analytics.loaded(integration, '<img src="http://example.com/example.png"/>');
     });
